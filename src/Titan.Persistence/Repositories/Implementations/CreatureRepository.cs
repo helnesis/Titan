4806701968417b59a22a2d0f6b32438f28045172
Kylian <stagiaire@mwp.local>
2024-12-10 16:03:50 +0100

    refact: rename class

--- src/Titan.Persistence/Repositories/Implementations/CreatureRepository.cs
+++ src/Titan.Persistence/Repositories/Implementations/CreatureRepository.cs
@@ -1,14 +1,12 @@
-﻿using Dapper;
-using MySqlConnector;
-using Titan.Domain.Entities;
+﻿using Titan.Domain.Entities;
 using Titan.Domain.Entities.Creatures;
 using Titan.Persistence.Repositories.Interfaces;
 
 namespace Titan.Persistence.Repositories.Implementations;
 
-public sealed class CreatureRepository(DatabaseConnectionMgr connectionMgr) : ICreatureRepository
+public sealed class CreatureRepository(MySqlDatabaseConnectionProvider databaseProvider) : ICreatureRepository
 {
-    public async Task CreateAsync(CreatureTemplate entity)
+    public Task CreateAsync(CreatureTemplate entity)
     {
         throw new NotImplementedException();
     }
