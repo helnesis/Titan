09c66fc767208e8794ee30801e819cd37f9fe1eb
Kylian <stagiaire@mwp.local>
2024-12-16 09:06:10 +0100

    add more flags

--- src/Titan.Domain/Enums/CreatureEnums.cs
+++ src/Titan.Domain/Enums/CreatureEnums.cs
@@ -247,6 +247,83 @@ public enum CreatureFamily : ushort
 }
 
 [Flags]
+public enum ExtraFlags : uint
+{
+    InstanceBind = 1 << 0,
+    
+    Civilian = 1 << 1,
+    
+    NoParry = 1 << 2,
+    
+    NoParryHasten = 1 << 3,
+    
+    NoBlock = 1 << 4,
+    
+    NoCrushingBlows = 1 << 5,
+    
+    NoXp = 1 << 6,
+    
+    Trigger = 1 << 7,
+    
+    NoTaunt = 1 << 8,
+    
+    NoMoveFlagsUpdate = 1 << 9,
+    
+    GhostVisibility = 1 << 10,
+    
+    UseOffHandAttack = 1 << 11,
+    
+    NoSellVendor = 1 << 12,
+    
+    CannotEnterCombat = 1 << 13,
+    
+    WorldEvent = 1 << 14,
+    
+    Guard = 1 << 15,
+    
+    IgnoreFeinDeath = 1 << 16,
+    
+    NoCrit = 1 << 17,
+    
+    NoSkillGains = 1 << 18,
+    
+    ObeysTauntDiminishingReturns = 1 << 19,
+    
+    AllDiminish = 1 << 20,
+    
+    NoPlayerDamageRequired = 1 << 21,
+    
+    Unused22 = 1 << 22,
+    
+    Unused23 = 1 << 23,
+    
+    Unused24 = 1 << 24,
+    
+    Unused25 = 1 << 25,
+    
+    Unused26 = 1 << 26,
+    
+    Unused27 = 1 << 27,
+    
+    DungeonBoss = 1 << 28,
+    
+    IgnorePathfinding = 1 << 29,
+    
+    ImmunityKnockback = 1 << 30,
+    
+    Unused31 = (uint)1 << 31,
+
+    // Masks
+    Unused = (Unused22 |
+                                                Unused23 | Unused24 | Unused25 |
+                                                Unused26 | Unused27 | Unused31),
+
+
+    DbAllowed = (0xFFFFFFFF & ~(Unused | DungeonBoss))
+}
+
+
+[Flags]
 public enum CreatureFlags : ulong
 {
     /// <summary>
@@ -489,4 +566,220 @@ public enum CreatureFlags : ulong
     /// The creature is a personal tabard designer
     /// </summary>
     PersonalTabardDesigner = (ulong)1 << 46,
+}
+
+[Flags]
+public enum CreatureUnitFlags : ulong
+{
+    ServerControlled = 1 << 0,
+    
+    NonAttackable = 1 << 1,
+    
+    RemoveClientControl = 1 << 2,
+    
+    PlayerControlled = 1 << 3,
+    
+    Rename = 1 << 4,
+    
+    Preparation = 1 << 5,
+    
+    Unknown6 = 1 << 6,
+    
+    NotAttackable1 = 1 << 7,
+    
+    ImmuneToPc = 1 << 8,
+    
+    ImmuneToNpc = 1 << 9,
+    
+    Looting = 1 << 10,
+    
+    PetInCombat = 1 << 11,
+    
+    PvpEnabling = 1 << 12,
+    
+    ForceNameplate = 1 << 13,
+    
+    CantSwim = 1 << 14,
+    
+    CanSwim = 1 << 15,
+    
+    NonAttackable2 = 1 << 16,
+    
+    Pacified = 1 << 17,
+    
+    Stunned = 1 << 18,
+    
+    InCombat = 1 << 19,
+    
+    OnTaxi = 1 << 20,
+    
+    Disarmed = 1 << 21,
+    
+    Confused = 1 << 22,
+    
+    Fleeing = 1 << 23,
+    
+    Possessed = 1 << 24,
+    
+    Uninteractible = 1 << 25,
+    
+    Skinnable = 1 << 26,
+    
+    Mount = 1 << 27,
+    
+    Unknown28 = 1 << 28,
+    
+    PreventEmotesFromChatText = 1 << 29,
+    
+    Sheathe = 1 << 30,
+    
+    Immune = (uint)1 << 31,
+
+    Disallowed = ServerControlled | NonAttackable | RemoveClientControl | PlayerControlled | Rename | Preparation | Unknown6 | NotAttackable1 | Looting | PetInCombat | PvpEnabling | CantSwim | CanSwim | NonAttackable2 | Pacified | Stunned | InCombat | OnTaxi | Disarmed | Confused | Fleeing | Possessed | Skinnable | Mount | Unknown28 | PreventEmotesFromChatText | Sheathe | Immune,
+
+    Allowed = 0xFFFFFFFF & ~Disallowed
+}
+
+[Flags]
+public enum CreatureUnitFlags2 : uint
+{
+    FeignDeath = 1 << 0,
+    
+    HideBody = 1 << 1,
+    
+    IgnoreReputation = 1 << 2,
+    
+    ComprehendLang = 1 << 3,
+    
+    MirrorImage = 1 << 4,
+    
+    DontFadeIn = 1 << 5,
+    
+    ForceMovement = 1 << 6,
+    
+    DisarmOffhand = 1 << 7,
+    
+    DisablePredStats = 1 << 8,
+    
+    AllowChangingTalents = 1 << 9,
+    
+    DisarmRanged = 1 << 10,
+    
+    RegeneratePower = 1 << 11,
+    
+    RestrictPartyInteraction = 1 << 12,
+    
+    PreventSpellClick = 1 << 13,
+    
+    InteractWhileHostile = 1 << 14,
+    
+    CannotTurn = 1 << 15,
+    
+    Unknown2 = 1 << 16,
+    
+    PlayDeathAnim = 1 << 17,
+    
+    AllowCheatSpells = 1 << 18,
+    
+    SuppressHighlightWhenTargetedOrMousedOver = 1 << 19,
+    
+    TreatAsRaidUnitForHelpfulSpells = 1 << 20,
+    
+    LargeAoi = 1 << 21,
+    
+    GiganticAoi = 1 << 22,
+    
+    NoActions = 1 << 23,
+    
+    AiWillOnlySwimIfTargetSwims = 1 << 24,
+    
+    DontGenerateCombatLogWhenEngagedWithNpcs = 1 << 25,
+    
+    UntargetableByClient = 1 << 26,
+    
+    AttackerIgnoresMinimumRanges = 1 << 27,
+    
+    UninteractibleIfHostile = 1 << 28,
+    
+    Unused11 = 1 << 29,
+    
+    InfiniteAoi = 1 << 30,
+    
+    Unused13 = (uint)1 << 31,
+
+    Disallowed = FeignDeath | IgnoreReputation | ComprehendLang | MirrorImage | ForceMovement | DisarmOffhand | DisablePredStats | AllowChangingTalents | DisarmRanged | RestrictPartyInteraction | CannotTurn | PreventSpellClick | AllowCheatSpells | SuppressHighlightWhenTargetedOrMousedOver | TreatAsRaidUnitForHelpfulSpells | LargeAoi | GiganticAoi | NoActions | AiWillOnlySwimIfTargetSwims | DontGenerateCombatLogWhenEngagedWithNpcs | AttackerIgnoresMinimumRanges | UninteractibleIfHostile | Unused11 | InfiniteAoi | Unused13,
+
+    Allowed = 0xFFFFFFFF & ~Disallowed
+}
+
+[Flags]
+public enum CreatureUnitFlags3 : uint
+{
+    Unknown0 = 1 << 0,
+    
+    UnconsciousOnDeath = 1 << 1,
+    
+    AllowMountedCombat = 1 << 2,
+    
+    GarrisonPet = 1 << 3,
+    
+    UiCanGetPosition = 1 << 4,
+    
+    AiObstacle = 1 << 5,
+    
+    AlternativeDefaultLanguage = 1 << 6,
+    
+    SuppressAllNpcFeedback = 1 << 7,
+    
+    IgnoreCombat = 1 << 8,
+    
+    SuppressNpcFeedback = 1 << 9,
+    
+    Unknown10 = 1 << 10,
+    
+    Unknown11 = 1 << 11,
+    
+    Unknown12 = 1 << 12,
+    
+    FakeDead = 1 << 13,
+    
+    NoFacingOnInteractAndFastFacingChase = 1 << 14,
+    
+    UntargetableFromUi = 1 << 15,
+    
+    NoFacingOnInteractWhileFakeDead = 1 << 16,
+    
+    AlreadySkinned = 1 << 17,
+    
+    SuppressAllNpcSounds = 1 << 18,
+    
+    SuppressNpcSounds = 1 << 19,
+    
+    AllowInteractionWhileInCombat = 1 << 20,
+    
+    Unknown21 = 1 << 21,
+    
+    DontFadeOut = 1 << 22,
+    
+    Unknown23 = 1 << 23,
+    
+    ForceHideNameplate = 1 << 24,
+    
+    Unknown25 = 1 << 25,
+    
+    Unknown26 = 1 << 26,
+    
+    Unknown27 = 1 << 27,
+    
+    Unknown28 = 1 << 28,
+    
+    Unknown29 = 1 << 29,
+    
+    Unknown30 = 1 << 30,
+    
+    Unknown31 = (uint)1 << 31,
+
+    Disallowed = Unknown0 | GarrisonPet | AlternativeDefaultLanguage | IgnoreCombat | SuppressNpcFeedback | Unknown10 | Unknown11 | Unknown12 | FakeDead | AlreadySkinned | AllowInteractionWhileInCombat | Unknown21 | Unknown23 | Unknown25 | Unknown26 | Unknown27 | Unknown28 | Unknown29 | Unknown30 | Unknown31,
+
+    Allowed = 0xFFFFFFFF & ~Disallowed
 }
