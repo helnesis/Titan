d8ce14ef1cd30a0a0b7497154e6c7e2f23efc5f9
Helnesis <kylian.nicolet@protonmail.com>
2025-11-23 21:44:13 +0100

    feat: update project(s) package(s).

--- src/Titan.Domain/Entities/Identifier.cs
+++ src/Titan.Domain/Entities/Identifier.cs
@@ -1,29 +1,23 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Text.Json.Serialization;
 
 namespace Titan.Domain.Entities;
 
 public readonly record struct Identifier(uint Value) : IParsable<Identifier>
 {
     public static readonly Identifier Min = uint.MinValue;
-
     public static Identifier Create(uint value) => value;
-
+    public static implicit operator Identifier(uint identifier) => new(identifier);
+    public static implicit operator uint(Identifier identifier) => identifier.Value;
     public static Identifier Parse(string s, IFormatProvider? provider)
         => uint.Parse(s);
-
     public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out Identifier result)
     {
         result = default;
 
-        if (!uint.TryParse(s: s, provider: provider, out uint value)) return false;
+        if (!uint.TryParse(s: s, provider: provider, out var value)) return false;
         
         result = new Identifier(value);
         
         return true;
     }
-
-    public static implicit operator Identifier(uint identifier) => new(identifier);
-
-    public static implicit operator uint(Identifier identifier) => identifier.Value;
 }
